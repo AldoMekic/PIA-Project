b604152b31e34679f402a66f99cebeed02eda840
AldoMekic <aldomekic@gmail.com>
2024-02-23 19:42:57 +0100

    Created login view and link towards it

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Login</title>
 </head>
 <body>
-    <h1>Hello Laravel</h1>
-    <a href="{{ route('login') }}">Login</a>
+    <h1>Login</h1>
 </body>
 </html>
